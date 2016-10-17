24cb413ffa7a08da39faced8e877e05e3fb6e3d9
kiyo <chidakiyo@gmail.com>
2016-10-17 23:41:14 +0900

    それっぽく動く状態

--- Main.go
+++ Main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/tarm/serial"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -27,22 +28,82 @@ func main() {
 	fmt.Printf("%#d\n", v)
 	if err != nil {
 		// TODO NOOP
+		fmt.Errorf("SKVER がダメ")
 	}
 
 	err = d.SKSETPWD(pwd)
 	if err != nil {
 		// TODO NOOP
+		fmt.Errorf("SKSETPWD がダメ")
 	}
 
 	err = d.SKSETRBID(rbID)
 	if err != nil {
 		// TODO NOOP
+		fmt.Errorf("SKSETRBID がダメ")
 	}
 
 	pan, err := d.SKSCAN()
 	fmt.Printf("%#d\n", pan)
 	if err != nil {
 		// TODO NOOP
+		fmt.Errorf("SKSCAN がダメ")
+	}
+
+	err = d.SKSREG("S2", pan.Channel)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Set PanID to S3 register...")
+	err = d.SKSREG("S3", pan.PanID)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Get IPv6 Addr with SKLL64...")
+	ipv6Addr, err := d.SKLL64(pan.Addr)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("IPv6 Addr is " + ipv6Addr)
+	fmt.Println("SKJOIN...")
+	err = d.SKJOIN(ipv6Addr)
+	if err != nil {
+		fmt.Println(err)
+	}
+	b := []byte{0x10, 0x81, 0x00, 0x01, 0x05, 0xFF, 0x01, 0x02, 0x88, 0x01, 0x62, 0x01, 0xE7, 0x00}
+	for {
+		fmt.Println("SKSENDTO...")
+		r, err := d.SKSENDTO("1", ipv6Addr, "0E1A", "1", b)
+		if err != nil {
+			fmt.Println(err)
+		}
+		a := strings.Split(r, " ")
+		if len(a) != 9 {
+			fmt.Println(r)
+		}
+		if a[7] != "0012" {
+			fmt.Println(fmt.Sprintf("%s is not 0012. ", a[7]))
+			continue
+		}
+		o := a[8]
+		w, err := strconv.ParseUint(o[len(o)-8:], 16, 0)
+		if err != nil {
+			fmt.Println(err)
+		}
+		t := time.Now()
+		fmt.Println(t, w)
+		data := map[string]string{
+			"timestamp": time.Time(t).UTC().Format("2006-01-02T15:04:05.000Z"),
+			"watt":      strconv.FormatUint(w, 10),
+		}
+		fmt.Printf("[OUTPUT] %v", data)
+		//err = logger.Post(*tag, data)
+		//if err != nil {
+		//	log.Error(err)
+		//}
+		time.Sleep(60 * time.Second)
 	}
 
 }
@@ -55,8 +116,9 @@ type Dongle struct {
 
 func NewDongle() *Dongle {
 	return &Dongle{
-		Baudrate:     115200,
-		SerialDevice: "/dev/ttyUSB0",
+		Baudrate: 115200,
+		//SerialDevice: "/dev/ttyUSB0",
+		SerialDevice: "/dev/tty.usbserial-A103BTKQ",
 	}
 }
 
@@ -166,4 +228,81 @@ func (b *Dongle) SKSCAN() (*PAN, error) {
 		}
 	}
 	return pan, nil
+}
+
+func (b *Dongle) SKSREG(k, v string) error {
+	err := b.write("SKSREG " + k + " " + v + "\r\n")
+	if err != nil {
+		return err
+	}
+	_, err = b.readUntilOK()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *Dongle) SKLL64(addr string) (string, error) {
+	err := b.write("SKLL64 " + addr + "\r\n")
+	if err != nil {
+		return "", err
+	}
+	reader := bufio.NewReader(b.Port)
+	r, _, err := reader.ReadLine()
+	if err != nil {
+		return "", err
+	}
+	fmt.Println(r)
+	r, _, err = reader.ReadLine()
+	if err != nil {
+		return "", err
+	}
+	fmt.Println(r)
+	return string(r), nil
+}
+
+func (b *Dongle) SKJOIN(ipv6Addr string) error {
+	err := b.write("SKJOIN " + ipv6Addr + "\r\n")
+	if err != nil {
+		return err
+	}
+	reader := bufio.NewReader(b.Port)
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		l := scanner.Text()
+		fmt.Print(l)
+		if strings.Contains(l, "FAIL ") {
+			return fmt.Errorf("Failed to SKJOIN. %s", l)
+		}
+		if strings.Contains(l, "EVENT 25 ") {
+			break
+		}
+	}
+	if scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	return nil
+}
+
+func (b *Dongle) SKSENDTO(handle, ipAddr, port, sec string, data []byte) (string, error) {
+	s := fmt.Sprintf("SKSENDTO %s %s %s %s %.4X ", handle, ipAddr, port, sec, len(data))
+	d := append([]byte(s), data[:]...)
+	d = append(d, []byte("\r\n")[:]...)
+	_, err := b.Port.Write(d)
+	if err != nil {
+		return "", err
+	}
+	reader := bufio.NewReader(b.Port)
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		l := scanner.Text()
+		fmt.Println(l)
+		if strings.Contains(l, "FAIL ") {
+			return "", fmt.Errorf("Failed to SKSENDTO. %s", l)
+		}
+		if strings.Contains(l, "ERXUDP ") {
+			return l, nil
+		}
+	}
+	return "", nil
 }
